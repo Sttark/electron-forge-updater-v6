5710db5bd9663d1ea6ff9e52b66cc42c9a806f44
Michael <mike@saymikeo.com>
2018-12-04 15:41:38 -0500

    update index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const {app, BrowserWindow, autoUpdater, dialog} = require('electron');
-import {enableLiveReload} from 'electron-compile';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -10,8 +9,6 @@ if (require('electron-squirrel-startup')) {
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
-
-const isDevMode = process.execPath.match(/[\\/]electron/);
 
 const createWindow = () => {
     // Create the browser window.
@@ -37,36 +34,36 @@ const createWindow = () => {
         mainWindow = null;
     });
 
-    if (!isDevMode) {
-        const server = 'https://hazel-server-jmcpkzydxa.now.sh';
-        const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
-
-        autoUpdater.setFeedURL(feed);
-
-        setInterval(() => {
-            autoUpdater.checkForUpdates();
-        }, 2000);
-
-        autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-            const dialogOpts = {
-                type: 'info',
-                buttons: ['Restart', 'Later'],
-                title: 'Application Update',
-                message: process.platform === 'win32' ? releaseNotes : releaseName,
-                detail:
-                    'A new version has been downloaded. Restart the application to apply the updates.'
-            };
-
-            dialog.showMessageBox(dialogOpts, response => {
-                if (response === 0) autoUpdater.quitAndInstall();
-            });
-        });
-
-        autoUpdater.on('error', message => {
-            console.error('There was a problem updating the application');
-            console.error(message);
-        });
-    }
+    // if (!isDevMode) {
+    //     const server = 'https://hazel-server-jmcpkzydxa.now.sh';
+    //     const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
+    //
+    //     autoUpdater.setFeedURL(feed);
+    //
+    //     setInterval(() => {
+    //         autoUpdater.checkForUpdates();
+    //     }, 2000);
+    //
+    //     autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    //         const dialogOpts = {
+    //             type: 'info',
+    //             buttons: ['Restart', 'Later'],
+    //             title: 'Application Update',
+    //             message: process.platform === 'win32' ? releaseNotes : releaseName,
+    //             detail:
+    //                 'A new version has been downloaded. Restart the application to apply the updates.'
+    //         };
+    //
+    //         dialog.showMessageBox(dialogOpts, response => {
+    //             if (response === 0) autoUpdater.quitAndInstall();
+    //         });
+    //     });
+    //
+    //     autoUpdater.on('error', message => {
+    //         console.error('There was a problem updating the application');
+    //         console.error(message);
+    //     });
+    // }
 };
 
 // This method will be called when Electron has finished
